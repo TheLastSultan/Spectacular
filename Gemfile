bac83c2ceda3f071ed3e332c16bd21d1b8edaf50
Meezo <mozeiny@gmail.com>
2018-10-04 20:22:12 -0700

    great spectacle seed data with faker

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'puma', '~> 3.11'
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
+gem  'faker'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'mini_racer', platforms: :ruby
 
@@ -39,6 +40,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,31 @@ User.create!(
     password_digest: "hello"
 )
 
-Spectacle.create!(
-    description: "The first seed",
-    title: "glasses",
-    staffpick: false 
-)
+50.times do |i|
+    possible_title= [Faker::Book.genre,
+                    Faker::Book.publisher, 
+                    Faker::BossaNova.song, 
+                    Faker::DragonBall.character].sample
+
+    possible_shapes = ["Round", "Oval", "Square"].sample
+    possible_material = ["Polycarbonate", "Mixed", "Metal"]
+    possible_sex = [true, false ].sample
+    possible_staffpick = [true, false].sample
+
+    Spectacle.create!(
+        color: Faker::Color.color_name, 
+        shape: possible_shapes,
+        description: Faker::Hipster.paragraph(2, true),
+        title: possible_title,
+        material: possible_material,
+        sex: possible_sex,
+        staffpick: possible_staffpick
+
+    )
+end
+
+
+
+
+
 
