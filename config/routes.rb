10ff53ec9a9b5b9c1f41b23539c885caa2430c0a
Meezo <mozeiny@gmail.com>
2018-10-06 13:07:03 -0700

    configured routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,15 @@ Rails.application.routes.draw do
   root to: 'static_pages#root'
 
   namespace :api, defaults: {format: :json} do
-      resources :spectacles, only: [:index, :show]
-      post 'pick', action: :pick, controller: 'spectacles'
+      
+    resources :users
+    resource :session, only: [:new, :create, :destroy ]  
+      
+    resources :spectacles, only: [:index, :show]
+    post 'pick', action: :pick, controller: 'spectacles'
+  
   end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
