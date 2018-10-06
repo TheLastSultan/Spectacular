ba4204826e2c7ee2cdbcceeeb66afa53823c8a8a
Meezo <mozeiny@gmail.com>
2018-10-06 13:25:22 -0700

    added authentication model for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,43 @@ class User < ApplicationRecord
     source: :spectacle
     
 
+    validates :username, :email, :session_token, :password_digest, presence: true
+    validates :username, :email, uniqueness: true
+    validates :password, length: {minimum: 6, allow_nil: true}
+    before_validation :ensure_session_token
+    
+    attr_reader :password
+    
+    def password=(pw)
+        @password = pw
+        self.password_digest = BCrypt::Password.create(pw)
+    end
+    
+    def is_password?(pw)
+        BCrypt::Password.new(password_digest).is_password?(pw)
+    end
+    
+    def self.find_by_email(email, pw)
+        user = User.find_by(email: email)
+        if user && user.is_password?(pw)
+        return user
+        end
+        nil
+    end
+    
+    def self.token
+        SecureRandom.urlsafe_base64
+    end
+    
+    def ensure_session_token
+        self.session_token ||= User.token
+    end
+    
+    def reset_session_token!
+        self.session_token = User.token
+        self.save
+        self.session_token
+    end
+
+
 end
