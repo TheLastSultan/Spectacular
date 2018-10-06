53b377e7eab31ced308f992b49aacf9376f9c212
Meezo <mozeiny@gmail.com>
2018-10-06 13:30:14 -0700

    added sessions_controller to users

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,26 +1,26 @@
 class Api::SessionsController < ApplicationController
 
-    def create
-      @user = User.find_by_email(
-        params[:user][:email],
-        params[:user][:password]
-      )
-      if @user
-        login(@user)
-        render :show
-      else
-        render json: ['Invalid Credentials'], status: 422
-      end
+  def create
+    @user = User.find_by_email(
+      params[:user][:email],
+      params[:user][:password]
+    )
+    if @user
+      login(@user)
+      render :show
+    else
+      render json: ['Invalid Credentials'], status: 422
     end
-  
-    def destroy
-      # debugger
-      if logout!
-        render json: {sessions: {id: nil}}
-      else
-        render json: ['No user to logout'], status: 404
-      end
-    end
-  
   end
+
+  def destroy
+    # debugger
+    if logout!
+      render json: {sessions: {id: nil}}
+    else
+      render json: ['No user to logout'], status: 404
+    end
+  end
+
+end
   
