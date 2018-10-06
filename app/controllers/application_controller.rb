d4cb94158e6ffd32731d090b237ce5eee70d36f8
Meezo <mozeiny@gmail.com>
2018-10-06 13:23:56 -0700

    added users authentiation controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,36 @@
 class ApplicationController < ActionController::Base
-end
+    skip_before_action :verify_authenticity_token
+    helper_method :current_user, :logged_in?
+  
+    def login(user)
+      session[:session_token] = user.reset_session_token!
+      @current_user = user
+    end
+  
+    def current_user
+      @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+  
+    def logged_in?
+      !!current_user
+    end
+  
+    def logout!
+      # debugger
+      if logged_in?
+        current_user.reset_session_token!
+        session[:session_token] = nil
+        @current_user = nil
+        return true
+      end
+      nil
+    end
+  
+    def require_login
+      unless logged_in?
+        # render :json {message: 'Bite Me'}, status: 500
+      end
+    end
+  
+  end
+  
