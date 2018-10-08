8578477e8ebd62f8cca06689d19af381e50bb37b
Meezo <mozeiny@gmail.com>
2018-10-07 18:25:18 -0700

    finished basic styling and added fontawesome icons

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import SpectacleIndexContainer from './spectacles/spectacles_index_container';
+import { HashRouter, Route } from 'react-router-dom';
+import Navbar from './sidecomponents/navbar';
+
+// Font Awesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faSearch, faShoppingCart)
 
 const App = () => (
   <div className="app">
-    <SpectacleIndexContainer/>
+    <HashRouter> 
+      <div> 
+        <Route  path="/" component={Navbar}/> 
+        <Route exact path="/" component={SpectacleIndexContainer}/>
+      </div> 
+    </HashRouter> 
   </div>
 );
 
---
+++ frontend/components/sidecomponents/navbar.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+
+// EYEGLASSES /browse_mens_glasses
+// SUNGLASSES /browse_womens_glasses
+// HOME TRY-ON /about_home_try_on
+// LOCATIONS /locations
+// SEARCH /search
+// HELP /help 
+// SIGN IN /signIn
+// Register /register 
+// Sign OUt /signOut
+
+// CART /cart
+
+
+
+const Navbar = () => (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">Spectacular</a>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav mr-auto">
+                <Link className="link-navbar" to={`#`}>
+                    <span className="nav-item">Eyeglasses</span>
+                </Link>
+                <Link className="link-navbar" to={`#`}>
+                    <span className="nav-item">Sunglasses</span>
+                </Link>  
+                <Link className="link-navbar" to={`#`}>
+                    <span className="nav-item">Home Try-On</span>
+                </Link>            
+            </ul>
+
+            <ul className="navbar-nav mr-auto">
+                <Link className="link-navbar" to={`#`}>
+                    <FontAwesomeIcon icon="search" className="faSearch" />
+                    <span className="nav-item">search</span>
+                </Link>
+                <Link className="link-navbar" to={`#`}>
+                    <span className="nav-item">sign-in | </span>
+                </Link>  
+                <Link className="link-navbar" to={`#`}>
+                    <span className="nav-item">cart</span>
+                    <FontAwesomeIcon icon="shopping-cart" className="faShoppingCart" />
+                </Link>               
+            </ul>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div> 
+    </nav> 
+);
+
+export default Navbar; 
