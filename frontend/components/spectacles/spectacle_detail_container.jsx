a7a83ffd1deead4db03083ff92bc27d1415f9c02
Meezo <mozeiny@gmail.com>
2018-10-07 19:17:44 -0700

    added description to set of show state

--- frontend/components/spectacles/spectacle_detail_container.jsx
+++ frontend/components/spectacles/spectacle_detail_container.jsx
@@ -1,4 +1,21 @@
+import { fetchSpectacles, fetchSpectacle } from '../../actions/spectacle_action';
 import { connect } from 'react-redux';
 import SpectacleIndex from './spectacles_index'
-import { fetchSpectacle } from '../../actions/spectacle_action';
+
+const mapStateToProps = (state, ownProps) => {
+    debugger; 
+    return {
+        spectacle: Object.keys(state.entities.spectacles).map(key => state.entities.spectacles[key]),
+        loading: state.ui.loading.detailLoading
+        }
+};
+
+const mapDispatchToProps = dispatch => {
+    return {fetchSpectacle: () => dispatch(fetchSpectacle())}
+};
+
+
+
+
+
 
