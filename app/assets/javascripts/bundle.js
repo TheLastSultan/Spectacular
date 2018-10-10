3aae89544e7e67b0a22f1d45a58edbddd6ce9eae
Meezo <mozeiny@gmail.com>
2018-10-09 19:46:12 -0700

    got the basic functionality working

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -481,13 +481,13 @@ function (_React$Component) {
       return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "container"
       }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_slick__WEBPACK_IMPORTED_MODULE_2___default.a, settings, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
-        src: "http://placekitten.com/g/400/200"
+        src: "https://c49.pcloud.com/dpZAYU9cpZ1v7rpHZ2OYNZZylyHI7Z3VZZtIZXZ5iFalQMSKGHTHitGQHAO6bdmefjV/th-8583747644-1074x396.png"
       })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
-        src: "http://placekitten.com/g/400/200"
+        src: "https://c277.pcloud.com/dpZRhU9cpZEv7rpHZ2OYNZZylyHI7Z3VZZtIZXZ8eShTRc9X8b6O0QyfwKMkk6yrXD7/th-8583747661-1066x409.png"
       })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
-        src: "http://placekitten.com/g/400/200"
+        src: "https://c282.pcloud.com/dpZgSU9cpZzi7rpHZ2OYNZZylyHI7Z3VZZtIZXZ6tWQHxlCv4uB1U03Tuvb5Rtbzw2X/th-8583747686-1064x414.png"
       })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
-        src: "http://placekitten.com/g/400/200"
+        src: "https://c6.pcloud.com/dpZXSU9cpZei7rpHZ2OYNZZylyHI7Z3VZZtIZXZCA3Ubw3TAtJdYLxLoOb2AFcdvvYk/th-8583747706-1080x406.png"
       }))));
     }
   }]);
