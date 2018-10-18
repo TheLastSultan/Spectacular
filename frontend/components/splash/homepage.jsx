9e30ca84cac9d110ab337998beb399a7881848a6
Meezo <mozeiny@gmail.com>
2018-10-18 13:32:38 -0700

    fixing database issue

---
+++ frontend/components/splash/homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+export default class SplashPage extends React.Component {
+    render() {
+        return (
+            <div className="homepage-container col-md-10">
+                <div className="Static-Header">
+                    <h1> Try 5 pairs for free </h1>
+                </div>
+                {/* <img src="" */}
+            
+        )
+    }
+}
