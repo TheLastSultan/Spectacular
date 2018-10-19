058e46bea94b9d97696d670b617502d009cfba7e
Ahmed Elzeiny <alfred.e@leantaas.com>
2018-10-19 11:44:56 -0700

    Align panda with XYZ axis, and center

--- frontend/components/session/avatar.js
+++ frontend/components/session/avatar.js
@@ -9,23 +9,17 @@ var WIDTH = 200,
 
 var camera = new THREE.PerspectiveCamera( 50, WIDTH/HEIGHT, 0.1, 1000 );
 
- const avatarEntryPoint = function(viewport) {
+const avatarEntryPoint = function(viewport) {
     var renderer = new THREE.WebGLRenderer({antialias:true});
     renderer.domElement.setAttribute("style", "border-radius: 50%;")
     renderer.setSize(WIDTH, HEIGHT);
     viewport.appendChild( renderer.domElement );
-    
-    camera.position.x = 1;
-    camera.position.z = -1;
 
-    camera.setRotationFromEuler(
-        new THREE.Euler(
-            3.115900480091839,
-            0.6176907036944816,
-            -3.126710749186239,
-            'XYZ'
-        )
-    );
+    // camera.position.x = 1;
+    camera.position.z = 1.7;
+
+    const axes = new THREE.AxisHelper( 100 );
+    scene.add( axes );
 
     var controls = new OrbitControls(camera, renderer.domElement);
 
@@ -37,27 +31,32 @@ var camera = new THREE.PerspectiveCamera( 50, WIDTH/HEIGHT, 0.1, 1000 );
                 .setMaterials(materials)
                 .load('./panda.obj', (object) => {
                     console.log(object);
+                    object.rotateY(3.14/180*-145);
+                    // rotateAroundObjectAxis(object, new Vector3(0, 1, 0), 0);
+                    object.position.z += 0.2;
+                    // object.position.x -= 0.2;
+                    // object.position.y += 0.1;
                     scene.add(object);
                 }, (e) => {console.log(e);}, () => {console.error(e);}); //, onProgress, onError 
         });
 
-    var ambientLight = new THREE.AmbientLight( "aliceblue", 1);
-    scene.add( ambientLight );
+    var ambientLight = new THREE.AmbientLight("aliceblue", 1);
+    scene.add(ambientLight);
 
-    var sceneLight = new THREE.PointLight("white");
-    scene.add(sceneLight);
+    // var sceneLight = new THREE.PointLight("white");
+    // scene.add(sceneLight);
 
     renderer.setClearColor("#03a9f4", 1);
-    
+
     var animate = function () {
-      requestAnimationFrame( animate );
-      controls.update();
-      sceneLight.position.set(-camera.position.x, -camera.position.y, -camera.position.z);
-      renderer.render(scene, camera);
-      console.log(camera.rotation);
+        requestAnimationFrame( animate );
+        controls.update();
+    //   sceneLight.position.set(-camera.position.x, -camera.position.y, -camera.position.z);
+        renderer.render(scene, camera);
+        console.log(camera.position, camera.rotation);
     };
-    
+
     animate();
- }
+}
 
  export default avatarEntryPoint;
